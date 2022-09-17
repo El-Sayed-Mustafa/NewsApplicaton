fb26364c8a7715de3e4fcb118d6da2b12cc62645
Elsayed mustafa <engelsayedmustafa123@gmail.com>
2022-09-17 17:04:57 +0200

    set up navigation app

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/UI/fragments/ArticalFragment.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.UI.fragments
+
+import androidx.fragment.app.Fragment
+import com.androiddevs.mvvmnewsapp.R
+
+class ArticalFragment :Fragment(R.layout.fragment_article){
+
+}
