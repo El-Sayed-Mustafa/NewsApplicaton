b88d026759674733233f2fdf95b37bdb99d2037a
Elsayed mustafa <engelsayedmustafa123@gmail.com>
2022-09-17 17:57:05 +0200

    Retrofit setup

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/Api/NewsApi.kt
@@ -0,0 +1,34 @@
+package com.androiddevs.mvvmnewsapp.Api
+
+import com.androiddevs.mvvmnewsapp.newsResponse
+import com.androiddevs.mvvmnewsapp.util.Constants.Companion.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("v2/top-headline")
+    suspend fun getBreakingNews(
+        @Query("country")
+        countryCode:String = "us",
+
+        @Query("page")
+        pageNumber: Int = 1,
+
+        @Query("apiKey")
+        apiKey:String = API_KEY
+    ): Response<newsResponse>
+
+    @GET("v2/everything")
+    suspend fun searchForNews(
+        @Query("q")
+        searchQuery:String,
+
+        @Query("page")
+        pageNumber: Int = 1,
+
+        @Query("apiKey")
+        apiKey:String = API_KEY
+    ): Response<newsResponse>
+}
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "7df739995f5f4bd7a26dece15b227c26"
+        const val BASE_URL ="https://newsapi.org"
+    }
+}
