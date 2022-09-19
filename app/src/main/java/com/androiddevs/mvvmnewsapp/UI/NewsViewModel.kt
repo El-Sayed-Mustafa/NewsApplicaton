1936c6b44b55c9ec63ae8523401a071e5c7a6007
Elsayed mustafa <engelsayedmustafa123@gmail.com>
2022-09-20 01:57:33 +0200

    Adding and Deleting Articles to our Room Database

--- app/src/main/java/com/androiddevs/mvvmnewsapp/UI/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/UI/NewsViewModel.kt
@@ -3,6 +3,7 @@ package com.androiddevs.mvvmnewsapp.UI
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.androiddevs.mvvmnewsapp.models.Article
 import com.androiddevs.mvvmnewsapp.models.newsResponse
 import com.androiddevs.mvvmnewsapp.repo.NewsRepo
 import com.androiddevs.mvvmnewsapp.util.Resource
@@ -10,23 +11,23 @@ import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class NewsViewModel(
-    val newsRepo : NewsRepo
+    val newsRepo: NewsRepo
 ) : ViewModel() {
 
-    val breakingNews :MutableLiveData<Resource<newsResponse>> = MutableLiveData()
+    val breakingNews: MutableLiveData<Resource<newsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
 
-    val searchNews :MutableLiveData<Resource<newsResponse>> = MutableLiveData()
+    val searchNews: MutableLiveData<Resource<newsResponse>> = MutableLiveData()
     var searchNewsPage = 1
 
     init {
-        getBreakingNews("eg")
+        getBreakingNews("us")
     }
 
-    fun getBreakingNews(countryCode: String)= viewModelScope.launch {
+    fun getBreakingNews(countryCode: String) = viewModelScope.launch {
         breakingNews.postValue(Resource.Loading())
-        val response = newsRepo.getBreakingNews(countryCode,breakingNewsPage)
-        breakingNews.postValue(handleBreakingNewsResponse( response))
+        val response = newsRepo.getBreakingNews(countryCode, breakingNewsPage)
+        breakingNews.postValue(handleBreakingNewsResponse(response))
     }
 
     fun searchNews(searchQuery: String) = viewModelScope.launch {
@@ -35,9 +36,9 @@ class NewsViewModel(
         searchNews.postValue(handleSearchNewsResponse(response))
     }
 
-    private fun handleBreakingNewsResponse(response: Response<newsResponse>):Resource<newsResponse>{
-        if (response.isSuccessful){
-            response.body()?.let {resultResponse->
+    private fun handleBreakingNewsResponse(response: Response<newsResponse>): Resource<newsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
                 return Resource.Success(resultResponse)
             }
         }
@@ -45,12 +46,22 @@ class NewsViewModel(
     }
 
 
-    private fun handleSearchNewsResponse(response: Response<newsResponse>):Resource<newsResponse>{
-        if (response.isSuccessful){
-            response.body()?.let {resultResponse->
+    private fun handleSearchNewsResponse(response: Response<newsResponse>): Resource<newsResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { resultResponse ->
                 return Resource.Success(resultResponse)
             }
         }
         return Resource.Error(response.message())
+    }
+
+    fun saveArticle(article: Article) = viewModelScope.launch {
+        newsRepo.upsert(article)
+    }
+
+    fun getSavedNews() = newsRepo.getSavedNews()
+
+    fun deleteArticle(article: Article) = viewModelScope.launch {
+        newsRepo.deleteArticle(article)
     }
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/repo/NewsRepo.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/repo/NewsRepo.kt
@@ -2,6 +2,7 @@ package com.androiddevs.mvvmnewsapp.repo
 
 import com.androiddevs.mvvmnewsapp.api.RetrofitInstance
 import com.androiddevs.mvvmnewsapp.db.ArticleDatabase
+import com.androiddevs.mvvmnewsapp.models.Article
 
 class NewsRepo(val db: ArticleDatabase) {
 
@@ -11,4 +12,10 @@ class NewsRepo(val db: ArticleDatabase) {
 
     suspend fun searchNews(searchQuery: String, pageNumber: Int) =
         RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
 }
